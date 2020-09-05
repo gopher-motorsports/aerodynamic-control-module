8b6e0064e5495a44293a57987f7db9575592052c
tyrre034 <tyrre034@umn.edu>
2020-09-04 20:32:10 -0700

    RTOS DRS progress made on updating parameters, speed arbitration, file structure, and structs

--- Core/Inc/GO4_ACM_2020.h
+++ Core/Inc/GO4_ACM_2020.h
@@ -12,6 +12,10 @@
 
 //*************DEFINES*************//
 #define BUFFER_SIZE 8
+#define NUM_PARAMETERS 6
+#define BUFFER_OVERFLOW_MODULO 7
+#define ERROR_THRESHOLD 10
+#define SIMILAR_THRESHOLD 5
 
 #define WHEEL_SPEED_UPPER_BOUND 100
 #define WHEEL_SPEED_LOWER_BOUND 0
@@ -36,14 +40,27 @@ typedef enum {
     IN_BOUNDS = 1,   // Current data HAS been used in a calculation - dont double count
 } IN_BOUNDS_STATE;
 
+typedef enum {
+	INOPERATIVE = 0,	// There are too many errors within the specific parameters error checking time frame
+	OPERATIVE = 1,		// There are not enough errors within the specific parameters error checking time frame to cause problems
+}PARAMETER_STATE;
+
+typedef enum {
+	MANUAL = 1,
+	AUTO = 0,
+} ACM_CONTROL_STATE;
+
 typedef struct {
 	U8 					current_value;			// received value
 	IN_BOUNDS_STATE 	in_bounds_flag;			// SET if value IN BOUNDS
 	U8 					buffer[BUFFER_SIZE];	// Ring buffer for parameter values
 	U8 					buffer_index;			// location in ring buffer
-	U8 					buffer_average;			// average buffer value
+	double 				buffer_average;			// average buffer value
 	U8					upper_bound;			// Max value
 	U8					lower_bound;			// Min value
+	U8 					error_count;			// Count how many times a value has been received out-of-bounds
+	PARAMETER_STATE		parameter_state;		// If there are too many errors
+
 }ACM_parameter;
 
 void ACM_Init(void);
--- Core/Src/GO4_ACM_2020.c
+++ Core/Src/GO4_ACM_2020.c
@@ -31,7 +31,9 @@ static ACM_parameter wheel_speed =
 		.buffer_index		= 0,
 		.buffer_average		= 0,
 		.upper_bound		= WHEEL_SPEED_UPPER_BOUND,
-		.lower_bound		= WHEEL_SPEED_LOWER_BOUND
+		.lower_bound		= WHEEL_SPEED_LOWER_BOUND,
+		.error_count		= 0,
+		.parameter_state	= OPERATIVE
 };
 
 static ACM_parameter air_speed =
@@ -42,7 +44,9 @@ static ACM_parameter air_speed =
 		.buffer_index		= 0,
 		.buffer_average		= 0,
 		.upper_bound		= AIR_SPEED_UPPER_BOUND,
-		.lower_bound		= AIR_SPEED_LOWER_BOUND
+		.lower_bound		= AIR_SPEED_LOWER_BOUND,
+		.error_count		= 0,
+		.parameter_state	= OPERATIVE
 };
 
 static ACM_parameter throttle_position =
@@ -53,7 +57,9 @@ static ACM_parameter throttle_position =
 		.buffer_index		= 0,
 		.buffer_average		= 0,
 		.upper_bound		= THROTTLE_POSITION_UPPER_BOUND,
-		.lower_bound		= THROTTLE_POSITION_LOWER_BOUND
+		.lower_bound		= THROTTLE_POSITION_LOWER_BOUND,
+		.error_count		= 0,
+		.parameter_state	= OPERATIVE
 };
 
 static ACM_parameter steering_angle =
@@ -64,7 +70,9 @@ static ACM_parameter steering_angle =
 		.buffer_index		= 0,
 		.buffer_average		= 0,
 		.upper_bound		= STEERING_ANGLE_UPPER_BOUND,
-		.lower_bound		= STEERING_ANGLE_LOWER_BOUND
+		.lower_bound		= STEERING_ANGLE_LOWER_BOUND,
+		.error_count		= 0,
+		.parameter_state	= OPERATIVE
 
 };
 
@@ -76,7 +84,9 @@ static ACM_parameter brake_pressure =
 		.buffer_index		= 0,
 		.buffer_average		= 0,
 		.upper_bound		= BRAKE_PRESSURE_UPPER_BOUND,
-		.lower_bound		= BRAKE_PRESSURE_LOWER_BOUND
+		.lower_bound		= BRAKE_PRESSURE_LOWER_BOUND,
+		.error_count		= 0,
+		.parameter_state	= OPERATIVE
 };
 
 static ACM_parameter acceleration =
@@ -87,34 +97,73 @@ static ACM_parameter acceleration =
 		.buffer_index		= 0,
 		.buffer_average		= 0,
 		.upper_bound		= ACCELERATION_UPPER_BOUND,
-		.lower_bound		= ACCELERATION_LOWER_BOUND
+		.lower_bound		= ACCELERATION_LOWER_BOUND,
+		.error_count		= 0,
+		.parameter_state	= OPERATIVE
 };
 
-static ACM_parameter parameters[] = {wheel_speed, air_speed, throttle_position, steering_angle,
-		brake_pressure, acceleration
-};
+//List of parameters for filtering and calculation
+ACM_parameter parameters[NUM_PARAMETERS];
 
 void ACM_Init(void) {
+
+	parameters[0] = wheel_speed;
+	parameters[1] = air_speed;
+	parameters[2] = throttle_position;
+	parameters[3] = steering_angle;
+	parameters[4] = brake_pressure;
+	parameters[5] = acceleration;
+
+
 	// Start Timers for CAN?
 	// Do the Wave
 }
 
 void fetch_data(void) {
-	ACM_parameter* current_parameter;
+	ACM_parameter* global_parameter_list;	// List of all of the parameters
+	U8 i;
+	U8 average_index;
+	U16 average_aggregate;
 
+	for(i = 0, global_parameter_list = parameters;i < NUM_PARAMETERS;i++) {
+		// Try to Receive Data over CAN
+		// global_parameter_list->current_value = fetch_parameter_function(specific parameter);
 
+		// Skip this parameter if it is INOPERATIVE (too many errors)
+		if(global_parameter_list->parameter_state != OPERATIVE) {
+			continue;
 
-	// Try to Receive Data over CAN
-	// Assign all _val variables here
+			// Possibly implement a restart feature??
+		}
 
-	if(1)
-	{
-		wheel_speed.in_bounds_flag = OUT_OF_BOUNDS;
-	} else {
-		wheel_speed.in_bounds_flag = IN_BOUNDS;
+		if ((global_parameter_list->current_value >= global_parameter_list->upper_bound) ||
+				(global_parameter_list->current_value <= global_parameter_list->lower_bound)) {
 
+			// Add current value to buffer
+			global_parameter_list->buffer[global_parameter_list->buffer_index] = global_parameter_list->current_value;	// Add current value to the buffer index of the specific variables buffer
+		} else {
+			//Add 1 to error count because current_value is out of bounds
+			global_parameter_list->error_count += 1;
 
+			// Render parameter INOPERATIVE if parameter has an error count that is too high
+			if (global_parameter_list->error_count >= ERROR_THRESHOLD) {
+				global_parameter_list->parameter_state = INOPERATIVE;
+			}
+		}
+
+		// add 1 to buffer and prevent buffer_index from going out of bounds
+		global_parameter_list->buffer_index += 1;
+		global_parameter_list->buffer_index = global_parameter_list->buffer_index % BUFFER_OVERFLOW_MODULO;
+
+		// Aggregate all buffer values to calculate average
+		for(average_index = 0;average_index < BUFFER_SIZE; average_index++) {
+			average_aggregate += global_parameter_list->buffer[average_index];
+		}
+
+		// Assign buffer average to specific "object"
+		global_parameter_list->buffer_average = average_aggregate / BUFFER_SIZE;
 	}
+	average_aggregate = 0;		// Reset aggregate variable to correctly calculate average for all parameters
 }
 
 void calculate_wing_angle(void) {
@@ -122,5 +171,28 @@ void calculate_wing_angle(void) {
 }
 
 void arbitrate_speed(void) {
+	double average_speed;
+	double previous_average_speed;
+	double acceleration;
+	ACM_CONTROL_STATE state;
+
+	if (air_speed.parameter_state == INOPERATIVE && wheel_speed.parameter_state == INOPERATIVE)
+	{
+		// Completely skunked
+		state = MANUAL;
+	} else {
+		state = AUTO;
+
+		// Determine which speed to use
+		if (air_speed.parameter_state == INOPERATIVE) {
+			average_speed = wheel_speed.buffer_average;
+		} else if (wheel_speed.parameter_state == INOPERATIVE) {
+			average_speed = air_speed.buffer_average;
+		} else {
+			average_speed = wheel_speed.buffer_average;
+		}
+
+		// Calculate acceleration in some way (derivative/difference of velocity and timer)
+	}
 
 }
